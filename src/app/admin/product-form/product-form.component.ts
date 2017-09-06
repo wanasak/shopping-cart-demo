89f84e22ead6a64d25f31191e6629d0a47e8c003
wanasak <u510610433@gmail.com>
2017-09-06 17:41:57 +0700

    add bootstrap card

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
@@ -11,12 +12,16 @@ export class ProductFormComponent {
 
   categories$;
 
-  constructor(private categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
   }
 
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
 }
