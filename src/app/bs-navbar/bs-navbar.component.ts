285dbf6f7e6af1c5d187a9419d529ec28cd2e1b9
wanasak <u510610433@gmail.com>
2017-09-06 13:45:25 +0700

    display current user

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,5 +1,6 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-bs-navbar',
@@ -8,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BsNavbarComponent {
 
+  user: firebase.User;
+
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(x => console.log(x));
+    afAuth.authState.subscribe(user => this.user = user);
   }
 
   logout() {
