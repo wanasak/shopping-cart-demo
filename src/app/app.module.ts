4978df1bed1646ff5fa8ab92bb8e935daeed087a
Wanasak Suraintaranggoon <u510610433@gmail.com>
2017-09-06 22:06:21 +0700

    display products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,7 +54,7 @@ import { ProductService } from './product.service';
     DataTableModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
+      { path: '', component: ProductsComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'login', component: LoginComponent },
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private productService: ProductService) {
+    this.products$ = productService.getAll();
   }
 
 }
