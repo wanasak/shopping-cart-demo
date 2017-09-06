4cfc608044e76c9fdabd666a1acdfeb18882fa7c
Wanasak Suraintaranggoon <u510610433@gmail.com>
2017-09-06 21:13:32 +0700

    config data table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { ProductService } from './../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { Product } from '../../models/product';
+import { DataTableResource } from 'angular-4-data-table/dist';
 
 @Component({
   selector: 'app-admin-products',
@@ -13,10 +14,32 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filterdProducts: Product[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
 
   constructor(private productService: ProductService) {
     this.subscription = productService.getAll()
-      .subscribe(products => this.filterdProducts = this.products = products);
+      .subscribe(products => {
+        this.filterdProducts = this.products = products;
+        this.initializeTable(products);
+      });
+  }
+
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset: 0 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
+
+  reloadItems(params) {
+    // tslint:disable-next-line:curly
+    if (!this.tableResource) return;
+
+    this.tableResource.query(params)
+      .then(items => this.items = items);
   }
 
   ngOnInit() {
