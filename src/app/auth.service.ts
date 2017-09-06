72e73c24626b67c8d81f735ddaabd07549e3fb50
wanasak <u510610433@gmail.com>
2017-09-06 16:11:00 +0700

    fixing bug

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,6 +6,9 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { ActivatedRoute } from '@angular/router';
 
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/switchMap';
+
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
@@ -30,7 +33,12 @@ export class AuthService {
 
   get appUser$(): Observable<AppUser> {
     return this.user$
-      .switchMap(user => this.userService.get(user.uid));
+      .switchMap(user => {
+        if (user) {
+          return this.userService.get(user.uid);
+        }
+        return Observable.of(null);
+      });
   }
 
 }
